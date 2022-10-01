79ca591013b15175fafc6cebca4e351f8024c406
Debian <debian@ramoukha-ansible.openstacklocal>
2022-10-01 13:04:21 +0000

    added a script to check if ansible is installed $ install it if not

---
+++ install_ansible.sh
@@ -0,0 +1,15 @@
+# this script gonna check if ansible is already install if isn't it's going to install it  
+
+# install ansible if not found debian 
+
+pkg=ansible
+Green='\033[0;32m'
+NC='\033[0m' # No Color
+
+status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
+if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
+  sudo apt install $pkg
+else
+	echo  "${Green}already installed${NC}"
+	ansible --version | head -n 1  
+fi
