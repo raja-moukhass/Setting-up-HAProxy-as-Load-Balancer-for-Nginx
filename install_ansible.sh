0d0c67de5b35942c5a0cf2e19e9d0fafd2c97d3e
Ubuntu <ubuntu@ip-172-31-9-208.ap-south-1.compute.internal>
2022-10-02 00:17:26 +0000

    move a setup role to check-setup role which i check if the tools are already installed if not to install them

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,8 @@
 # this script gonna check if ansible is already install if isn't it's going to install it  
 
 # install ansible if not found debian 
+sudo apt-get update
+sudo apt-get upgrade
 
 pkg=ansible
 Green='\033[0;32m'
@@ -8,7 +10,7 @@ NC='\033[0m' # No Color
 
 status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
 if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-  sudo apt install $pkg
+  sudo apt install -y  $pkg
 else
 	echo  "${Green}already installed${NC}"
 	ansible --version | head -n 1  
