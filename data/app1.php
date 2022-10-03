82ab187a9de10cd5595895060fd570adb3530845
raja moukhass <moraja858@gmail.com>
2022-10-03 16:48:30 +0100

    add php to docker-compose.yml

---
+++ data/app1.php
@@ -0,0 +1,10 @@
+
+<html>
+    <head>
+        <title>PHP Hello World!</title>
+    </head>
+    <body>
+        <?php echo '<h1>Hello World from php</h1>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
