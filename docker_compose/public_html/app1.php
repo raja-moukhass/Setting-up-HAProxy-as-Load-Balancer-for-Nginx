196198f0463a7de6584c7622e65b2c6ae07cffc9
raja moukhass <moraja858@gmail.com>
2022-10-04 11:13:19 +0100

    fixed some errors and got the php applications to work with nginx vhosts

---
+++ docker_compose/public_html/app1.php
@@ -0,0 +1,11 @@
+
+<html>
+    <head>
+        <title>PHP Hello World!</title>
+    </head>
+    <body>
+        <?php echo '<h1>hello from application 1 </h1>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
+
---
+++ docker_compose/public_html/app2.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP Hello World!</title>
+    </head>
+    <body>
+        <?php echo '<h1>hello from application 2 </h1>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
+
